326af5572fb0b820a9e863f9826b99e282e56ae1
freqyt <freqbusiness@gmail.com>
2026-08-21 16:17:10 +0200

    Update Windows catalogue via Admin Dashboard

--- data/windows.js
+++ data/windows.js
@@ -55,7 +55,8 @@ const windowsData = {
         {
           "name": "Nexus 5 Complete",
           "price": 80,
-          "includes": "Including the full 20,000+ preset library"
+          "includes": "Including the full 20,000+ preset library",
+          "tags": "reFX"
         },
         {
           "name": "Kontakt 8 Starter Bundle",
@@ -74,7 +75,8 @@ const windowsData = {
           "includes": "ACE, Bazille, Diva, Hive, Repro-1 & 5, Zebra3",
           "badges": [
             "NEW"
-          ]
+          ],
+          "tags": "u-He"
         }
       ],
       "tier40": [
