c1d2c7c295335cde8d2379148a79759bb6615c4f
freqyt <freqbusiness@gmail.com>
2026-08-21 06:09:12 +0200

    Save encrypted GitHub token

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Plugin Catalogue — Config
-// Master password hash for Sounds List Admin Dashboard
 const CATALOGUE_CONFIG = {
-  passwordHash: "08b3fe0a195f77f4fa7f217df7c4492ff6790c1907436353bd16b920230e27b9",
-  catalogueName: "Sounds List"
+  "passwordHash": "08b3fe0a195f77f4fa7f217df7c4492ff6790c1907436353bd16b920230e27b9",
+  "catalogueName": "Sounds List",
+  "encryptedGitHubToken": "MaM/UsUM0HTEt7ccXGcknnlgQbeeC8VwDnhn3mZHQY3HWl2soPH/nNgx3KIvCDFEip7ThUxp4DWPfZozIXSxEgUG8fQ1zE+wyGZFOvGE0whChlH9NLmFNT26lWXGF93mCnJjsi6JiOYmK/E7reTCOl2n2CGOvqlXUg=="
 };
